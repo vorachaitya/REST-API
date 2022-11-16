79c9ccacb9589a1b6b63416a44cf8b6bf2ba75fc
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2022-11-16 17:19:54 +0530

    added search feature

--- models/driver.js
+++ models/driver.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//create geolocation schema
+const GeoSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point",
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere",
+  },
+});
+
 //create driver Schema and model
 const DriverSchema = new Schema({
   name: {
@@ -14,8 +26,9 @@ const DriverSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  geometry: GeoSchema,
 });
 
-const Driver = mongoose.model("driver", DriverSchema);
+const Driver = mongoose.model("USER", DriverSchema);
 
 module.exports = Driver;
